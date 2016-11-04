9521eb66d729f51d92c19cb47ecce51803fd8a3c
Silun Wang <igeekwang@gmail.com>
2016-11-04 20:43:36 +0000

    best configurations

--- train_model.sh
+++ train_model.sh
@@ -14,9 +14,9 @@ th -i $ABS/summary/train.lua -titleDir  $OUT_DIR/train/title/ \
  -articleDir  $OUT_DIR/train/article/ \
  -modelFilename  $MDL_DIR/$2 \
  -miniBatchSize  64 \
- -embeddingDim  64 \
+ -embeddingDim  200 \
  -bowDim  200 \
- -hiddenSize  64 \
+ -hiddenSize  400 \
  -epochs  20 \
  -learningRate 0.1 \
  -validArticleDir  $OUT_DIR/valid.filter/article/ \
