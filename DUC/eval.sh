111bc56014de59b30f967174c97f4a9f13ac4fc9
Silun Wang <igeekwang@gmail.com>
2016-11-04 20:41:01 +0000

    why do you have so many bugs?

--- DUC/eval.sh
+++ DUC/eval.sh
@@ -10,8 +10,8 @@ mkdir -p $1/tmp_SYSTEM
 python $ABS/DUC/make_rouge.py --base $1 --gold tmp_GOLD --system tmp_SYSTEM --input input.txt
 perl $ABS/DUC/prepare4rouge-simple.pl tmp_SYSTEM tmp_GOLD tmp_OUTPUT
 
-cd tmp_OUTPUT
-export PERL5LIB=/data/users/sashar/summary/duc/RELEASE-1.5.5/
+cd $1/tmp_OUTPUT
+export PERL5LIB=/home/ubuntu/RELEASE-1.5.5/
 
 echo "FULL LENGTH"
 perl $ROUGE/ROUGE-1.5.5.pl -m -n 2 -w 1.2 -e $ROUGE -a settings.xml
